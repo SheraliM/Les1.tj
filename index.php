ce627a3f45d2651bac39e4125aff9778c1495caf
Sherali <emichfun@gmail.com>
2019-02-07 17:54:50 +0500

    add all files from my project

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE HTML>
+<html> 
+    <head>
+        <title>Document</title>
+
+        <meta charset = "UTF-8">
+
+        <link rel = "stylesheet" href = "css/bootstrap.css">
+
+    </head>
+
+<body>
+    
+
+<?php
+require 'nav.php';
+require 'slider.php';
+?>
+
+<div style = "padding:100px; background-image:url(https://thewallpaper.co/wp-content/uploads/2016/10/Glitter-blue-light-wallpaper-HD-cool-images-high-definition-artwork-tablet-mac-desktop-images-widescreen-display-digital-photos-1920x1080.jpg);position:relative;">
+
+
+<div class="card text-center">
+  <div class="card-header">
+    Featured
+  </div>
+  <div class="card-body" >
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  <div class="card-footer text-muted">
+    2 days ago
+  </div>
+</div>
+
+
+
+<div style="padding:20px; background-color: #e2e2e2">
+<div class="row">
+  <div class="col-lg-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  
+  <div class="col-lg-4 ">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row" style = "margin-top:20px">
+  <div class="col-lg-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  
+  <div class="col-lg-4 ">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+
+<?php
+
+require 'footer.php';
+?>
+    <script src="js/jquery-3.3.1.min.js"> </script>
+    <script src="js/popper.min.js"></script>
+    <script src = "js/bootstrap.min.js"></script>
+
+
+</body>
+    </html>
---
+++ slider.php
@@ -0,0 +1,51 @@
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img height="600px" width = "1200px" src="https://avatars.mds.yandex.net/get-pdb/51720/2459b5e9-90dc-4aa0-b0a7-100edd4de64d/s1200?webp=false" class="d-block w-100" alt="Img ">
+      <div class="carousel-caption d-none d-md-block">
+    <h1>This is First slider</h1>
+    <h5>Example</h5>
+    <button type="button" class="btn btn-primary btn-lg">Folow link</button>
+  </div>
+    </div>
+    <div class="carousel-item">
+      <img  height="600px" width = "1200px"  src="https://www.davidcosgrove.com/wp-content/uploads/2016/11/home-02-slide-01.jpg" class="d-block w-100" alt="IMG">
+      <div class="carousel-caption d-none d-md-block">
+    <h1>This is Second slider</h1>
+    <h5>Example</h5>
+    
+    <button type="button" class="btn btn-primary btn-lg">Folow link</button>
+  </div>
+    </div>
+    <div class="carousel-item">
+      <img  height="600px" width = "1200px" src="https://im0-tub-ru.yandex.net/i?id=690529276c376b9b810b4edc859b218b-l&n=13" class="d-block w-100" alt="Img">
+    
+      <div class="carousel-caption d-none d-md-block">
+    <h1>This is Third slider</h1>
+    <h5>Example</h5>
+    
+  </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<div class="carousel-item">
+  <img src="..." alt="...">
+  <div class="carousel-caption d-none d-md-block">
+    <h5>...</h5>
+    <p>...</p>
+  </div>
+</div>
